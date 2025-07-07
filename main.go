1631a38d092f56466b01ed8f7dd7da01a0536bcd
TimLai666 <tim930102@icloud.com>
2025-07-07 22:49:16 +0800

    新增 API 路由以處理發送電子郵件請求

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +12,14 @@ func main() {
 	r.Static("/mail", "mail")
 	r.StaticFile("/", "apps_index/dist/index.html")
 	r.Static("/assets", "apps_index/dist/assets")
+
+	apiGp := r.Group("/api")
+	{
+		apiGp.GET("/send-email", func(c *gin.Context) {
+			http.Post("/SendEmail", "application/json", c.Request.Body)
+			c.JSON(http.StatusOK, gin.H{"status": "email sent"})
+		})
+	}
 
 	r.Run(":8080")
 }
